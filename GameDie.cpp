b4807a6f4525149d55e111f4d009a7ddfac29637
pagrawal1407 <pagrawal@mail.csuchico.edu>
2016-01-29 10:37:02 -0800

    First commit

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+   srand(1);
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+   int number = rand();
+   cout << number;
 }
